b661e1ef85d1b72b4a313756de0b7577f4c0e06c
bprest <brian.prest@duke.edu>
2015-02-08 09:42:42 -0500

    BP Draft Assignment Code v2

--- BPCleaningCode.py
+++ BPCleaningCode.py
@@ -3,53 +3,70 @@ from pandas import Series, DataFrame
 import pandas as pd
 import numpy as np
 import os
+import xlrd
 
-main_dir = u'C:/Users/bcp17/Google Drive/THE RAGDOLLS_ 590 Big Data/CER_Data/CER Electricity Revised March 2012/UnzippedData'
+main_dir = u'C:/Users/bcp17/Google Drive/THE RAGDOLLS_ 590 Big Data/CER_Data/CER Electricity Revised March 2012'
+data_dir = main_dir+"/UnzippedData/"
 git_dir = "C:/Users/bcp17/OneDrive/Grad School/GitHub/PubPol590"
+assignmentfile = "SME and Residential allocations.csv"
 
 # Read first 10 lins of the file to determine type.
+# This reveals that it's space delimited
 N = 10
 with open(main_dir + "/" + "File1.txt") as myfile:
     head = [next(myfile) for x in xrange(N)]
 print head
-# It's space delimited
 
-df_dict = dict()
-for i in range(1,7):
-    df_dict[i] = pd.read_csv(main_dir + "/" + "File" + str(i) +".txt", sep = " ", names = ['panid', 'time','consump'], header=None, na_values=['-','NA'])
-print("done!")
-df = pd.concat([df_dict[1], df_dict[2], df_dict[3], df_dict[4], df_dict[5], df_dict[6]], ignore_index=True)
-#df1 = pd.read_csv(main_dir+"/"+"File1.txt", sep = " ", names = ['panid', 'time','consump'], header=None)
-#df2 = pd.read_csv(main_dir+"/"+"File2.txt", sep = " ", names = ['panid', 'time','consump'], header=None)
-#df3 = pd.read_csv(main_dir+"/"+"File3.txt", sep = " ", names = ['panid', 'time','consump'], header=None)
-#df4 = pd.read_csv(main_dir+"/"+"File4.txt", sep = " ", names = ['panid', 'time','consump'], header=None)
-#df5 = pd.read_csv(main_dir+"/"+"File5.txt", sep = " ", names = ['panid', 'time','consump'], header=None)
-#df6 = pd.read_csv(main_dir+"/"+"File6.txt", sep = " ", names = ['panid', 'time','consump'], header=None)
-#df = pd.concat([df1, df2, df3, df4, df5, df6], ignore_index = True)
-#del [df1, df2, df3, df4, df5, df6]
 
-df=df_dict[1] # temporary for code testing.
-del df_dict
-df.dtypes
-sum(df.duplicated()) # No duplicates
+# Read in the data.
+pathlist = [data_dir + v for v in os.listdir(data_dir) if v.startswith("File")]
+list_of_dfs = [ pd.read_csv(v, names = ['panid', 'date', 'kwh'], sep = " ", header=None, na_values=['-','NA']) for v in pathlist]
+
+# Now clean the data
+df=pd.read_csv(data_dir+"File1.txt", names = ['panid', 'date', 'kwh'], sep = " ", header=None, na_values=['-','NA']) # temporary, for debugging code.
+
+# Determine number of fully duplicated rows.
+print("Number of Fully Duplicated Rows: ") 
+print(sum(df.duplicated())) 
+# No full duplicates
+
+# Determine duplicates on Panid & Time.
 dupe_on_panidtime_tb = df.duplicated(['panid','time'])
 dupe_on_panidtime_bt = df.duplicated(['panid','time'], take_last=True)
-sum(dupe_on_panidtime_tb)
+
+# Print Number of Duplicated Rows (on Panid & Time)
+print("Number of Duplicated Rows on Panid & Time: ")
+print(sum(dupe_on_panidtime_tb))
+
+# Print duplicated values
+print("Duplicates on Panid & Time: ")
+print(df[dupe_on_panidtime_bt | dupe_on_panidtime_tb])
+# Ack! There are at least 12 duplicate values (same panid, same time). Only take the last set of these.
 
 # drop duplicated values, taking only the last.
 df = df.drop_duplicates(['panid','date'], take_last=True)
 
-# check for NaNs. Drop full row if any is missing.
+# check for NaNs. Drop full row if any column is missing.
 isnan = np.isnan(df)
+print("Number of NaNs:",sum(isnan))
 anyisnan = sum(isnan, axis = 1)>0 # True if any column is missing.
 df = df[~anyisnan] # keep only the ones where no column is missing.
 
-# Load in identifiers.
+# Fix daylight savings issues:
+#* Day 452 has 2 missing entries numbered 2 and 3
+#* Day 669 has 2 extra entries numbered 49 and 50
+#* Day 298 has 2 extra entries numbered 49 and 50
+# Fix these by dropping observations 4-5, and pulling 6-50 all back by 2.
 
 
+# Load in treatment assignment info.
+assignment = pd.read_csv(main_dir+"/"+assignmentfile, sep = ",", na_values=[' ','-','NA'], usecols = range(0,4))
+assignment = assignment[assignment.Code==1] # keep only the residential guys
+assignment = assignment[[0,2,3]] # drop "Code" now, since it's always 1.
+assignment.columns = ['panid','tariff','stimulus']
 
-pd.merge(df1,df2, on = ['panid'])
-
+# Merge with panel data.
+mergeddf = pd.merge(df,assignment, on = ['panid'])
 
 print("done!")
     
