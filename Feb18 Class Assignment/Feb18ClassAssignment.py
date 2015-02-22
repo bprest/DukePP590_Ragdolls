28c3e3a51a40a9a375500dbf1f73b224f591eb6a
bprest <brian.prest@duke.edu>
2015-02-22 15:08:22 -0500

    Sunday Update

--- Feb18 Class Assignment/Feb18ClassAssignment.py
+++ Feb18 Class Assignment/Feb18ClassAssignment.py
@@ -10,19 +10,40 @@ print(time.ctime())
 main_dir = u'C:/Users/bcp17/Google Drive/THE RAGDOLLS_ 590 Big Data/CER_Data/CER Electricity Revised March 2012'
 data_dir = main_dir+"/UnzippedData/"
 assignmentfile = "SME and Residential allocations.xlsx"
+timeseriescorrection = "timeseries_correction.csv"
 
 ## Read in the data
 # Make a list of file paths for each of the file and then read it in as a dataframe
 pathlist = [data_dir + "File" + str(v) + ".txt" for v in range(1,7)]
-df = pd.concat([pd.read_table(v, sep = " ", names = ['panid','time','kwh']) for v in pathlist], ignore_index = True)
-#df = pd.concat([pd.read_table(v, sep = " ", names = ['panid','time','kwh'], nrows = 1.5*10**6) for v in pathlist], ignore_index = True)
+#df = pd.concat([pd.read_table(v, sep = " ", names = ['panid','time','kwh']) for v in pathlist], ignore_index = True)
+df = pd.concat([pd.read_table(v, sep = " ", names = ['panid','time','kwh'], nrows = 1.5*10**6) for v in pathlist], ignore_index = True)
 
 # Group variables on panid and day, then sum consumption across each day.
-df['day'] = (df.time - (df.time % 100))/100
-hourgrp = df.groupby(['panid','day'])
+df['hour'] = (df.time % 100)
+df['day'] = (df.time - df['hour'])/100
+
+# Pull in timestamps
+tscorr = pd.read_csv(main_dir+"/"+timeseriescorrection, header=0, parse_dates=[1])
+tscorr = tscorr[['ts','hour_cer','day_cer']]
+tscorr.rename(columns={'hour_cer': 'hour', 'day_cer': 'day'}, inplace=True)
+
+df = pd.merge(df,tscorr, on=['day','hour'])
+del tscorr
+del [[df['time'],df['day'],df['hour']]]
+
+# Create year, month, day variables
+df['year'] = df['ts'].apply(lambda x: x.year)
+df['month'] = df['ts'].apply(lambda x: x.month)
+df['day'] = df['ts'].apply(lambda x: x.day)
+
+# Aggregate on day
+daygrp = df.groupby(['panid','year','month','day'])
+df_daily= daygrp['kwh'].sum().reset_index()
+
+# Aggregate on month
+monthgrp = df.groupby(['panid','year','month'])
+df_monthly = monthgrp['kwh'].sum().reset_index()
 del df
-df_daily = DataFrame(zip(hourgrp.panid.mean(),hourgrp.day.mean(),hourgrp['kwh'].sum()), columns = ['panid','day','kwh'])
-del hourgrp
 
 # Load in treatment assignment info.
 assignment = pd.read_excel(main_dir+"/"+assignmentfile, sep = ",", na_values=[' ','-','NA'], usecols = range(0,4))
@@ -38,34 +59,66 @@ assignment = assignment[keeprows]
 
 # Merge with panel data.
 df_daily = pd.merge(df_daily,assignment, on = ['panid'])
+df_monthly = pd.merge(df_monthly,assignment, on = ['panid'])
 del [assignment, keeprows]
 
 # Group on treatment status and day
-grp = df_daily.groupby(['tariff','day'])
+grp_daily = df_daily.groupby(['tariff','year','month','day'])
+trt_daily = {(k[1],k[2],k[3]): df_daily.kwh[v].values for k,v in grp_daily.groups.iteritems() if k[0]=="A"} 
+ctrl_daily = {(k[1],k[2],k[3]): df_daily.kwh[v].values for k,v in grp_daily.groups.iteritems() if k[0]=="E"}
+#del [df_daily, grp_daily]
 
-trt = {k[1]: df_daily.kwh[v].values for k,v in grp.groups.iteritems() if k[0]=="A"} 
-ctrl = {k[1]: df_daily.kwh[v].values for k,v in grp.groups.iteritems() if k[0]=="E"}
-del [df_daily, grp]
+# Group on treatment status and month
+grp_monthly = df_monthly.groupby(['tariff','year','month'])
+trt_monthly = {(k[1],k[2]): df_monthly.kwh[v].values for k,v in grp_monthly.groups.iteritems() if k[0]=="A"} 
+ctrl_monthly = {(k[1],k[2]): df_monthly.kwh[v].values for k,v in grp_monthly.groups.iteritems() if k[0]=="E"}
 
-keys = trt.keys()
+keys_daily = trt_daily.keys()
+keys_monthly = trt_monthly.keys()
 
 # create dataframes of tstats over time
-tstats = DataFrame([(k, np.abs(ttest_ind(trt[k],ctrl[k], equal_var=False)[0])) for k in keys], columns=['time','tstat'])
-del [trt, ctrl, keys]
-tstats.sort(['time'], inplace=True)
-tstats.reset_index(inplace=True, drop=True)
+tstats_daily = DataFrame([(k[0], k[1], k[2], np.abs(ttest_ind(trt_daily[k],ctrl_daily[k], equal_var=False)[0])) for k in keys_daily], columns=['year','month','day','tstat'])
+pvals_daily  = DataFrame([(k[0], k[1], k[2], np.abs(ttest_ind(trt_daily[k],ctrl_daily[k], equal_var=False)[1])) for k in keys_daily], columns=['year','month','day','pval'])
+t_p_daily = pd.merge(tstats_daily,pvals_daily)
+t_p_daily.sort(['year','month','day'], inplace=True)
+t_p_daily.reset_index(inplace=True, drop=True)
+
+tstats_monthly = DataFrame([(k[0], k[1], np.abs(ttest_ind(trt_monthly[k],ctrl_monthly[k], equal_var=False)[0])) for k in keys_monthly], columns=['year','month','tstat'])
+pvals_monthly  = DataFrame([(k[0], k[1], np.abs(ttest_ind(trt_monthly[k],ctrl_monthly[k], equal_var=False)[1])) for k in keys_monthly], columns=['year','month','pval'])
+t_p_monthly = pd.merge(tstats_monthly,pvals_monthly)
+t_p_monthly.sort(['year','month'], inplace=True)
+t_p_monthly.reset_index(inplace=True, drop=True)
+
+del [trt_daily, ctrl_daily,trt_monthly, ctrl_monthly, keys_daily, keys_monthly]
 
 # Plotting -----------------------------------------
 fig1 = plt.figure()
-ax1 = fig1.add_subplot(1,1,1) 
-ax1.plot(tstats['tstat'])
+ax1 = fig1.add_subplot(2,1,1) 
+ax1.plot(t_p_monthly.tstat)
 ax1.axhline(2, color='red', linestyle="--")
-ax1.set_title('t-stats over time')
+ax1.set_title('Monthly t-stats over time')
+ax2 = fig1.add_subplot(2,1,2) 
+ax2.plot(t_p_monthly.pval)
+ax2.axhline(0.05, color='red', linestyle="--")
+ax2.set_title('Monthly p-values over time')
 plt.show()
 
-tstats.to_csv(main_dir + "/tstats_daily.csv", sep = ',')
+fig2 = plt.figure()
+ax3 = fig2.add_subplot(2,1,1) 
+ax3.plot(t_p_daily.tstat)
+ax3.axhline(2, color='red', linestyle="--")
+ax3.set_title('Daily t-stats over time')
+ax4 = fig2.add_subplot(2,1,2) 
+ax4.plot(t_p_daily.pval)
+ax4.axhline(0.05, color='red', linestyle="--")
+ax4.set_title('Daily p-values over time')
+plt.show()
 
-share_over_two = sum(tstats.tstat>2)/tstats.tstat.count()
-print("Share of tstats>2 is " + str(share_over_two))
+#tstats.to_csv(main_dir + "/tstats_daily.csv", sep = ',')
+
+share_under_five_pct_daily   = sum(t_p_daily.pval<0.05)/t_p_daily.pval.count()
+share_under_five_pct_monthly = sum(t_p_monthly.pval<0.05)/t_p_monthly.pval.count()
+print("Share of daily p-values<0.05 is " + str(share_under_five_pct_daily))
+print("Share of monthly p-values<0.05 is " + str(share_under_five_pct_monthly))
 print("done!")
 print(time.ctime())
