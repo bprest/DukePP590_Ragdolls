8a87a994e738b9217c50740409d7123f1ac2df70
bprest <brian.prest@duke.edu>
2015-02-22 16:50:38 -0500

    Sunday Update 2: Both graphs
    includes monthly

--- Feb18 Class Assignment/Feb18ClassAssignment.py
+++ Feb18 Class Assignment/Feb18ClassAssignment.py
@@ -15,8 +15,25 @@ timeseriescorrection = "timeseries_correction.csv"
 ## Read in the data
 # Make a list of file paths for each of the file and then read it in as a dataframe
 pathlist = [data_dir + "File" + str(v) + ".txt" for v in range(1,7)]
-#df = pd.concat([pd.read_table(v, sep = " ", names = ['panid','time','kwh']) for v in pathlist], ignore_index = True)
-df = pd.concat([pd.read_table(v, sep = " ", names = ['panid','time','kwh'], nrows = 1.5*10**6) for v in pathlist], ignore_index = True)
+df = pd.concat([pd.read_table(v, sep = " ", names = ['panid','time','kwh']) for v in pathlist], ignore_index = True)
+#df = pd.concat([pd.read_table(v, sep = " ", names = ['panid','time','kwh'], nrows = 1.5*10**6) for v in pathlist], ignore_index = True)
+
+# Load in treatment assignment info.
+assignment = pd.read_excel(main_dir+"/"+assignmentfile, sep = ",", na_values=[' ','-','NA'], usecols = range(0,4))
+assignment = assignment[assignment.Code==1] # keep only the residential guys
+assignment = assignment[[0,2,3]] # drop "Code" now, since it's always 1.
+assignment.columns = ['panid','tariff','stimulus']
+
+# keep only the control guys (E,E) or guys with tariff A & bi-monthly only stimulus (A,1).
+keeprows = ((assignment.tariff =="E") & (assignment.stimulus == "E")) | ((assignment.tariff == "A") & (assignment.stimulus == "1"))
+
+# trim out all others
+assignment = assignment[keeprows]
+
+# Merge with panel data.
+df = pd.merge(df,assignment, on = ['panid'])
+#df_monthly = pd.merge(df_monthly,assignment, on = ['panid'])
+del [assignment, keeprows]
 
 # Group variables on panid and day, then sum consumption across each day.
 df['hour'] = (df.time % 100)
@@ -37,41 +54,26 @@ df['month'] = df['ts'].apply(lambda x: x.month)
 df['day'] = df['ts'].apply(lambda x: x.day)
 
 # Aggregate on day
-daygrp = df.groupby(['panid','year','month','day'])
+daygrp = df.groupby(['panid','tariff','year','month','day'])
 df_daily= daygrp['kwh'].sum().reset_index()
 
 # Aggregate on month
-monthgrp = df.groupby(['panid','year','month'])
+monthgrp = df.groupby(['panid','tariff','year','month'])
 df_monthly = monthgrp['kwh'].sum().reset_index()
 del df
-
-# Load in treatment assignment info.
-assignment = pd.read_excel(main_dir+"/"+assignmentfile, sep = ",", na_values=[' ','-','NA'], usecols = range(0,4))
-assignment = assignment[assignment.Code==1] # keep only the residential guys
-assignment = assignment[[0,2,3]] # drop "Code" now, since it's always 1.
-assignment.columns = ['panid','tariff','stimulus']
-
-# keep only the control guys (E,E) or guys with tariff A & bi-monthly only stimulus (A,1).
-keeprows = ((assignment.tariff =="E") & (assignment.stimulus == "E")) | ((assignment.tariff == "A") & (assignment.stimulus == "1"))
-
-# trim out all others
-assignment = assignment[keeprows]
-
-# Merge with panel data.
-df_daily = pd.merge(df_daily,assignment, on = ['panid'])
-df_monthly = pd.merge(df_monthly,assignment, on = ['panid'])
-del [assignment, keeprows]
+del [daygrp, monthgrp]
 
 # Group on treatment status and day
 grp_daily = df_daily.groupby(['tariff','year','month','day'])
 trt_daily = {(k[1],k[2],k[3]): df_daily.kwh[v].values for k,v in grp_daily.groups.iteritems() if k[0]=="A"} 
 ctrl_daily = {(k[1],k[2],k[3]): df_daily.kwh[v].values for k,v in grp_daily.groups.iteritems() if k[0]=="E"}
-#del [df_daily, grp_daily]
+del [df_daily, grp_daily]
 
 # Group on treatment status and month
 grp_monthly = df_monthly.groupby(['tariff','year','month'])
 trt_monthly = {(k[1],k[2]): df_monthly.kwh[v].values for k,v in grp_monthly.groups.iteritems() if k[0]=="A"} 
 ctrl_monthly = {(k[1],k[2]): df_monthly.kwh[v].values for k,v in grp_monthly.groups.iteritems() if k[0]=="E"}
+del [df_monthly, grp_monthly]
 
 keys_daily = trt_daily.keys()
 keys_monthly = trt_monthly.keys()
@@ -90,16 +92,19 @@ t_p_monthly.sort(['year','month'], inplace=True)
 t_p_monthly.reset_index(inplace=True, drop=True)
 
 del [trt_daily, ctrl_daily,trt_monthly, ctrl_monthly, keys_daily, keys_monthly]
+del [tstats_daily, tstats_monthly, pvals_daily, pvals_monthly]
 
 # Plotting -----------------------------------------
 fig1 = plt.figure()
 ax1 = fig1.add_subplot(2,1,1) 
 ax1.plot(t_p_monthly.tstat)
 ax1.axhline(2, color='red', linestyle="--")
+ax1.axvline(x=6,ymin=0, ymax=3, color='green', linestyle="--")
 ax1.set_title('Monthly t-stats over time')
 ax2 = fig1.add_subplot(2,1,2) 
 ax2.plot(t_p_monthly.pval)
 ax2.axhline(0.05, color='red', linestyle="--")
+ax2.axvline(x=6,ymin=0, ymax=1, color='green', linestyle="--")
 ax2.set_title('Monthly p-values over time')
 plt.show()
 
@@ -107,14 +112,17 @@ fig2 = plt.figure()
 ax3 = fig2.add_subplot(2,1,1) 
 ax3.plot(t_p_daily.tstat)
 ax3.axhline(2, color='red', linestyle="--")
+ax3.axvline(x=172, color='green', linestyle="--")
 ax3.set_title('Daily t-stats over time')
 ax4 = fig2.add_subplot(2,1,2) 
 ax4.plot(t_p_daily.pval)
 ax4.axhline(0.05, color='red', linestyle="--")
+ax4.axvline(x=172, color='green', linestyle="--")
 ax4.set_title('Daily p-values over time')
 plt.show()
 
-#tstats.to_csv(main_dir + "/tstats_daily.csv", sep = ',')
+t_p_daily.to_csv(main_dir + "/tstats_daily.csv", sep = ',')
+t_p_monthly.to_csv(main_dir + "/tstats_monthly.csv", sep = ',')
 
 share_under_five_pct_daily   = sum(t_p_daily.pval<0.05)/t_p_daily.pval.count()
 share_under_five_pct_monthly = sum(t_p_monthly.pval<0.05)/t_p_monthly.pval.count()
